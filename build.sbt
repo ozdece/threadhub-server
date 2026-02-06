49bacb4b9edccae9638cdd1fe5642b23db4816a3
Cem Özden <ozdenc64@gmail.com>
2026-02-06 21:58:07 +0100

    Adding plugins and other stuff

--- build.sbt
+++ build.sbt
@@ -1,8 +1,14 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.18"
-
 lazy val root = (project in file("."))
   .settings(
-    name := "threadhub-server"
+    name := "threadhub-server",
+    semanticdbEnabled := true,
+    semanticdbVersion := scalafixSemanticdb.revision,
+    scalaVersion := "2.13.18",
+    scalacOptions := List("-Yrangepos", "-Wunused:imports")
   )
+
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "2.4.0"
+
+addCommandAlias("lint", "scalafixAll;scalafmtAll;")
--- src/main/scala/com/ozdece/threadhub/server/Application.scala
+++ src/main/scala/com/ozdece/threadhub/server/Application.scala
@@ -1,5 +1,48 @@
 package com.ozdece.threadhub.server
 
+import java.time.ZonedDateTime
+
+sealed trait RSSUpdatePeriod
+
+object RSSUpdatePeriod {
+  case object Hourly extends RSSUpdatePeriod
+}
+
+sealed trait RSS20Properties
+
+final case class SyndicationHint(updatePeriod: RSSUpdatePeriod, updateFrequency: Int)
+    extends RSS20Properties
+
+sealed trait ItemProperty
+
+final case class DcProperty(creator: String) extends ItemProperty
+
+final case class Image(url: String, title: String, link: String, width: Int, height: Int)
+
+//TODO: Add comments field later
+final case class Item(
+    id: String,
+    title: String,
+    link: Option[String],
+    description: Option[String],
+    author: Option[String],
+    category: Seq[String],
+    pubDate: Option[ZonedDateTime],
+    properties: Seq[ItemProperty]
+)
+
+final case class RSS20(
+    id: String,
+    title: String,
+    link: String,
+    description: String,
+    lastBuildDate: Option[ZonedDateTime],
+    ttl: Option[Int],
+    image: Option[Image],
+    properties: Seq[RSS20Properties],
+    items: Seq[Item]
+)
+
 object Application extends App {
   println("Hello World!")
 }
